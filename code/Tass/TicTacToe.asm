dadb4fb4ee20f4e8de8af2819ab616bca0e21f6a
Mimonsi <konstantin-schwarze@outlook.de>
2025-12-03 05:22:50 +0100

    WIP Main Menu

--- code/Tass/TicTacToe.asm
+++ code/Tass/TicTacToe.asm
@@ -23,12 +23,17 @@ VIA_ACR   = VIA_BASE+$B
 VIA_PCR   = VIA_BASE+$C
 VIA_IFR   = VIA_BASE+$D
 
+STRPTR    = $D0                 ; Pointer to string (2 bytes)
+SCRPTR    = $D2                 ; Pointer to screen (2 bytes)
+
 KEYROW0   = $DA                 ; Keyboard row 0
 KEYROW1   = $DB                 ; Keyboard row 1
 KEYROW2   = $DC                 ; Keyboard row 2
 KEYROW3   = $DD                 ; Keyboard row 3
 KEYROW4   = $DE                 ; Keyboard row 4
 KEYROW5   = $DF                 ; Keyboard row 5
+
+PRGLEN    = $D8                 ; Length of the program in memory
 
 ; Program header for Cody Basic's loader (needs to be first)
 
@@ -41,8 +46,6 @@ KEYROW5   = $DF                 ; Keyboard row 5
 
 .LOGICAL    ADDR                ; The actual program gets loaded at ADDR
 
-MAIN        LDX #0              ; The program starts running from here
-
             
 ;_LOOP       LDA TEXT,X          ; Copies TEXT into screen memory (kept for reference)
 ;            BEQ _DONE
@@ -52,39 +55,82 @@ MAIN        LDX #0              ; The program starts running from here
 ;            INX
 ;            BRA _LOOP
 
+MAIN        STZ PRGLEN          ; Clear program length
+            STZ PRGLEN+1
+            
+            JSR SHOWSCRN
+            
+_LOOP       JSR KEYSCAN         ; Scan the keyboard
 
-_LOOP       LDA INTRO_TEXT,X         ; Display intro text
-            JSR KEYSCAN         ; Scan the keyboard
-
-            LDA KEYROW0         ; Pressed Q for 1
+            LDA KEYROW0         ; Pressed Q for quit?
             AND #%00001
-            BNE _K1
+            BNE _QUIT
             
-            LDA KEYROW0         ; Pressed W for 2
+            LDA KEYROW1         ; Pressed L for load?
             AND #%10000
-            BNE _K2
+            BNE _LOAD
             
-            LDA KEYROW0         ; Pressed E for 3
+            LDA KEYROW5         ; Pressed P for program?
             AND #%10000
-            BNE _K3
+            BNE _PROG
             
             BRA _LOOP           ; Repeat main loop
-    
-_K1         LDA #$31           ; ASCII '1'
-            STA SCRRAM + 0      ; Place '1' at position 1
-            BRA _LOOP           ; Repeat main loop
-
-_K2         LDA #$32           ; ASCII '2'
-            STA SCRRAM + 1      ; Place '2' at position 2
-            BRA _LOOP           ; Repeat main loop
-
-_K3         LDA #$33           ; ASCII '3'
-            STA SCRRAM + 2      ; Place '3' at position 3
-            BRA _LOOP           ; Repeat main loop
             
-_DONE       JMP _DONE           ; Loops forever
+_QUIT       RTS                 ; Return to BASIC
+            
+_LOAD       
+            BRA _LOOP
+            
+_PROG       
+            BRA _LOOP
 
-; KEYSCAN (taken from codyprog.asm)
+
+;
+; SHOWSCRN
+;
+; Shows the main screen.
+;
+SHOWSCRN  JSR CLRSCRN
+            
+          LDX #0
+          LDY #0
+          JSR MOVESCRN
+          
+          LDX #MSG_TICTACTOE
+          JSR PUTMSG
+          
+          LDX #0
+          LDY #1
+          JSR MOVESCRN
+          
+          LDX #MSG_SUBTITLE
+          JSR PUTMSG
+
+          LDX #0
+          LDY #3
+          JSR MOVESCRN
+          
+          LDX #MSG_MENUINSTRUCTIONS
+          JSR PUTMSG
+
+          LDX #0
+          LDY #4
+          JSR MOVESCRN
+          
+          LDX #MSG_MENUPLAY
+          JSR PUTMSG
+          
+          LDX #0
+          LDY #5
+          JSR MOVESCRN
+          
+          LDX #MSG_MENUQUIT
+          JSR PUTMSG
+          
+          RTS
+
+;
+; KEYSCAN
 ;
 ; Scans the keyboard matrix (so that key selections for menu options can be detected).
 ;
@@ -111,8 +157,189 @@ _LOOP       LDA VIA_IORA          ; Read the keys for the current row from the V
             PLA
             
             RTS
+
+
+;
+; MOVESCRN
+;
+; Moves the SCRPTR to the position for the column/row in the X and Y
+; registers. All registers are clobbered by the routine.
+;
+MOVESCRN  LDA #<SCRRAM            ; Move screen pointer to beginning
+          STA SCRPTR+0
+          LDA #>SCRRAM
+          STA SCRPTR+1
+          
+          INY                     ; Increment pointer for each row
+_LOOPY    CLC 
+          LDA SCRPTR+0
+          ADC #40
+          STA SCRPTR+0
+          LDA SCRPTR+1
+          ADC #0
+          STA SCRPTR+1
+          DEY
+          BNE _LOOPY
+          
+          CLC                     ; Add position on column
+          TXA
+          ADC SCRPTR+0
+          STA SCRPTR+0
+          LDA SCRPTR+1
+          ADC #0
+          STA SCRPTR+1
+          
+          RTS
+
+;
+; CLRSCRN
+;
+; Clear the entire screen by filling it with whitespace (ASCII 20 decimal).
+;
+CLRSCRN   LDA #<SCRRAM            ; Move screen pointer to beginning
+          STA SCRPTR+0
+          LDA #>SCRRAM
+          STA SCRPTR+1
+          
+          LDA #20                 ; Clear screen by filling with whitespaces
+          
+          LDY #25                 ; Loop 25 times on Y
+          
+_LOOPY    LDX #40                 ; Loop 40 times on X for each Y
+          
+_LOOPX    STA (SCRPTR)            ; Store zero
+
+          INC SCRPTR+0            ; Increment screen position
+          BNE _NEXT
+          INC SCRPTR+1
+          
+_NEXT     DEX                     ; Next X
+          BNE _LOOPX
+          
+          DEY                     ; Next Y
+          BNE _LOOPY
+          
+          RTS
+
+
+;
+; PUTMSG
+;
+; Puts a message string (one of the MSG_XXX constants) on the screen.
+;
+PUTMSG      PHA
+            PHY
             
-INTRO_TEXT        .NULL "Welcome to Tic Tac Toe!"       ; TEXT as a null-terminated string
+            LDA MSGS_L,X        ; Load the pointer for the string to print
+            STA STRPTR+0
+            LDA MSGS_H,X
+            STA STRPTR+1
+            
+            LDY #0
+            
+_LOOP       LDA (STRPTR),Y      ; Read the next character (check for null)
+            BEQ _DONE
+            
+            JSR PUTCHR          ; Copy the character and move to next
+            INY         
+            
+            BRA _LOOP           ; Next loop
+            
+_DONE       PLY
+            PLA
+            
+            RTS
+
+;
+; PUTCHR
+;
+; Puts an individual ASCII character on the screen.
+;
+PUTCHR      STA (SCRPTR)        ; Copy the character   
+            
+            INC SCRPTR+0        ; Increment screen position
+            BNE _DONE
+            INC SCRPTR+1
+          
+_DONE       RTS
+
+;
+; IDs for the message strings that can be displayed in the program.
+;
+MSG_TICTACTOE  = 0
+MSG_SUBTITLE  = 1
+MSG_MENUINSTRUCTIONS = 2
+MSG_MENUPLAY  = 3
+MSG_MENUQUIT  = 4
+MSG_WAITBINA  = 5
+MSG_WAITREPE  = 6
+MSG_RECVDATA  = 7
+MSG_PROGDATA  = 8
+MSG_VERIDATA  = 9
+MSG_VERIFYOK  = 10
+MSG_VERIFYBAD = 11
+MSG_LENGTH    = 12
+MSG_CLEAR     = 13
+MSG_ERROR     = 14
+
+;
+; The strings displayed by the program.
+;
+STR_TICTACTOE  .NULL "Tic Tac Toe"
+STR_SUBTITLE  .NULL "Welcome to Tic Tac Toe!"
+STR_MENUINSTRUCTIONS .NULL "Press Q-O keys to select position."
+STR_MENUPLAY  .NULL "(P)lay"
+STR_MENUQUIT  .NULL "(Q)uit"
+STR_WAITBINA  .NULL "Waiting for binary data..."
+STR_WAITREPE  .NULL "Waiting for repeat data to verify..."
+STR_RECVDATA  .NULL "Receiving data..."
+STR_PROGDATA  .NULL "Programming data..."
+STR_VERIDATA  .NULL "Verifying data..."
+STR_VERIFYOK  .NULL "Verify OK."
+STR_VERIFYBAD .NULL "Verify FAILED."
+STR_LENGTH    .NULL "Length: $"
+STR_CLEAR     .NULL "                                    "
+STR_ERROR     .NULL "ERROR"
+
+;
+; Low bytes of the string table addresses.
+;
+MSGS_L
+  .BYTE <STR_TICTACTOE
+  .BYTE <STR_SUBTITLE
+  .BYTE <STR_MENUINSTRUCTIONS
+  .BYTE <STR_MENUPLAY
+  .BYTE <STR_MENUQUIT
+  .BYTE <STR_WAITBINA
+  .BYTE <STR_WAITREPE
+  .BYTE <STR_RECVDATA
+  .BYTE <STR_PROGDATA
+  .BYTE <STR_VERIDATA
+  .BYTE <STR_VERIFYOK
+  .BYTE <STR_VERIFYBAD
+  .BYTE <STR_LENGTH
+  .BYTE <STR_CLEAR
+  .BYTE <STR_ERROR
+
+;
+; High bytes of the string table addresses.
+;
+MSGS_H
+  .BYTE >STR_TICTACTOE
+  .BYTE >STR_SUBTITLE
+  .BYTE >STR_MENUINSTRUCTIONS
+  .BYTE >STR_MENUPLAY
+  .BYTE >STR_MENUQUIT
+  .BYTE >STR_WAITBINA
+  .BYTE >STR_WAITREPE
+  .BYTE >STR_RECVDATA
+  .BYTE >STR_PROGDATA
+  .BYTE >STR_VERIDATA
+  .BYTE >STR_VERIFYOK
+  .BYTE >STR_VERIFYBAD
+  .BYTE >STR_LENGTH
+  .BYTE >STR_CLEAR
+  .BYTE >STR_ERROR
 
 LAST                            ; End of the entire program
 
